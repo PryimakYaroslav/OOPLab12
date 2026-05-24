81a620d038d59ab1bfa954bacf944c5b7a86e027
PryimakYaroslav <anonimusua6@gmail.com>
2026-05-24 12:22:03 +0300

    Add Find and Print functions

--- main.cpp
+++ main.cpp
@@ -22,6 +22,33 @@ void insertBST(std::unique_ptr<BinaryNode>& root, int val){
     }
 }
 
+BinaryNode* searchBST(const std::unique_ptr<BinaryNode>& root, int val, int& comparsions) {
+    if (!root) return nullptr;
+
+    comparsions++;
+    if (root->data == val){
+        return root.get();
+    }
+
+    comparsions++;    
+    if (val < root->data) {
+        return searchBST(root->left, val, comparsions);
+    } else {
+        return searchBST(root->right, val, comparsions);        
+    }
+}
+
+void findAndPrint(const std::unique_ptr<BinaryNode>& root, int val) {
+    int compr = 0;
+    BinaryNode* result = searchBST(root, val, compr);
+
+    if (result) {
+        std::cout << "Element " << val << " has been found." << " Comparsions: " << compr << "\n";
+    } else {
+        std::cout << "Element " << val << " not found." << " Comparsions: " << compr << "\n";        
+    }
+}
+
 int main() {
     std::unique_ptr<BinaryNode> bstRoot = nullptr;
 
@@ -30,5 +57,10 @@ int main() {
         insertBST(bstRoot, x);
     }
 
+    findAndPrint(bstRoot, 30);
+    findAndPrint(bstRoot, 40);
+    findAndPrint(bstRoot, 65);  
+    findAndPrint(bstRoot, 80);
+    findAndPrint(bstRoot, 50); 
     return 0;
 }
